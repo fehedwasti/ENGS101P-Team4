9ba6466ead3bf96e31ed6d060299bbca2d473047
badamski61 <33897950+badamski61@users.noreply.github.com>
2017-11-22 21:19:46 +0000

    Add files via upload

---
+++ Stirring/Frequency_generator2/Frequency_generator2.ino
@@ -0,0 +1,11 @@
+int PWM = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(PWM, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(PWM, 150);
+}
