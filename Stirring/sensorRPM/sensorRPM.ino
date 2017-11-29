1da1cefb67887b178059ee572a63c4f0edf764d7
fehedwasti <fehedwasi@gmail.com>
2017-11-29 12:41:47 +0000

    Update sensorRPM

--- Stirring/sensorRPM/sensorRPM.ino
+++ Stirring/sensorRPM/sensorRPM.ino
@@ -1,4 +1,4 @@
-int sensorPin = 3; // Change value if necessary
+int sensorPin = 3; // P1.1
 
 void setup() {
   Serial.begin(9600);
@@ -6,22 +6,40 @@ void setup() {
 }
 
 void loop() {
-  int time;
+  float time;
   int sensorStatus = digitalRead(sensorPin);
-  int initialStatus = sensorStatus;
+  int initialStatus = sensorStatus;    //Initialise inital status
   
-      int t0 = millis();  //Start timer
-      while (sensorStatus == initialStatus){
-        sensorStatus = digitalRead(sensorPin);
-      }
-    
-      int t1 = millis();  //Stop timer once status has changed.
-      
-      time = t1 - t0; //Time for sensorStatus to change
-      Serial.print("TIME TAKEN: ");
-      Serial.println(time);
-      
-  //This time is equivalent to 1/4 of a revolution.
-  //Convert to rpm using sensor_rpm = 60/(4*time) = 15/time
-  int sensor_rpm = 15/time;
+  
+  while (sensorStatus == initialStatus){    //Waits until state changes
+    sensorStatus = digitalRead(sensorPin);  //Status != Initial
+
+  }
+  int oppositeStatus = sensorStatus ;   //Opposite to initialStatus
+  
+  float t0 = micros();  //Start timer
+  
+  for (int i = 0; i < 2; i++){ 
+    while (sensorStatus != initialStatus){
+      sensorStatus = digitalRead(sensorPin);
+    }
+    initialStatus = oppositeStatus;
+  }
+  float t1 = micros();  //Stop timer once status has changed.
+  
+  time = t1 - t0; //Time for sensorStatus to change
+  if (time == 0){
+     int a = 1;    //Doesn't print if time = 0.
+  }
+  else {
+    Serial.print("TIME TAKEN (microseconds): ");
+    Serial.println(time);
+    time = time / pow(10,6); //convert from micros to seconds.
+        
+    //This time is equivalent to 1/4 of a revolution.
+    //Convert to rpm using sensor_rpm = 60/(4*time) = 15/time
+    long sensor_rpm = 60/time;
+    Serial.print("RMP: ");
+    Serial.println(sensor_rpm);
+  }
 }
