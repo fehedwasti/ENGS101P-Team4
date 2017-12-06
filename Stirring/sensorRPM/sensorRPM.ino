f62eb7c914795bf282de1bf0669d02800821e8e4
fehedwasti <fehedwasi@gmail.com>
2017-12-06 11:44:52 +0000

    2nd TRY

--- Stirring/sensorRPM/sensorRPM.ino
+++ Stirring/sensorRPM/sensorRPM.ino
@@ -1,45 +1,83 @@
 int sensorPin = 3; // P1.1
+int readings[3];
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensorPin, INPUT);
 }
 
-void loop() {
-  float time;
-  int sensorStatus = digitalRead(sensorPin);
-  int initialStatus = sensorStatus;    //Initialise inital status
-  
-  
-  while (sensorStatus == initialStatus){    //Waits until state changes
-    sensorStatus = digitalRead(sensorPin);  //Status != Initial
 
+int rootms(int list[3]) {    //Function calculates the average of a list
+  int rms = 0;
+  for (int i = 0; i < 3; i++){
+    rms += list[i];
   }
-  int oppositeStatus = sensorStatus ;   //Opposite to initialStatus
+  rms = rms / 3;
+  return rms;
   
-  float t0 = micros();  //Start timer
   
-  for (int i = 0; i < 2; i++){ 
-    while (sensorStatus != initialStatus){
-      sensorStatus = digitalRead(sensorPin);
+}
+
+
+void loop() {
+  while (readings[3] == 0) {
+    
+    int iteration = 0;
+    
+    float time;
+    int sensorStatus = digitalRead(sensorPin);
+    int initialStatus = sensorStatus;    //Initialise inital status
+    
+    
+    while (sensorStatus == initialStatus){    //Waits until state changes
+      sensorStatus = digitalRead(sensorPin);  //Status != Initial
+  
     }
-    initialStatus = oppositeStatus;
-  }
-  float t1 = micros();  //Stop timer once status has changed.
+    int oppositeStatus = sensorStatus ;   //Opposite to initialStatus
+    
+    float t0 = micros();  //Start timer
+    
+    for (int i = 0; i < 2; i++){ 
+      while (sensorStatus != initialStatus){
+        sensorStatus = digitalRead(sensorPin);
+      }
+      initialStatus = oppositeStatus;
+    }
+    float t1 = micros();  //Stop timer once status has changed.
+    
+    time = t1 - t0; //Time for sensorStatus to change
+    if (time == 0){
+       int a = 1;    //Doesn't print if time = 0.
+    }
+    else {
+      readings[iteration] = time;
+      iteration += 1;
+    }
+  }    //EXIT WHILE LOOP
   
-  time = t1 - t0; //Time for sensorStatus to change
-  if (time == 0){
-     int a = 1;    //Doesn't print if time = 0.
+  //Test values for code
+  /*readings[0] = 4;
+  readings[1] = 5;
+  readings[2] = 6;*/
+  
+  Serial.print("[");
+  for (int i = 0; i < 3; i++) {
+    
+    Serial.print(readings[i]);
+    Serial.print(" ");
   }
-  else {
-    Serial.print("TIME TAKEN (microseconds): ");
-    Serial.println(time);
-    time = time / pow(10,6); //convert from micros to seconds.
-        
-    //This time is equivalent to 1/4 of a revolution.
-    //Convert to rpm using sensor_rpm = 60/(4*time) = 15/time
-    long sensor_rpm = 60/time;
-    Serial.print("RMP: ");
-    Serial.println(sensor_rpm);
-  }
+  Serial.println("]");
+  
+
+  int averageTime = rootms(readings);
+  
+  Serial.println(averageTime);  //IN MICROSECONDS
+  
+   averageTime = averageTime / pow(10,6); //convert from micros to seconds.        
+      //This time is equivalent to time for one revolution.
+      //Convert to rpm using sensor_rpm = 60/time
+      
+      long sensor_rpm = 60/averageTime;
+      Serial.print("RPM: ");
+      Serial.println(sensor_rpm);
 }
